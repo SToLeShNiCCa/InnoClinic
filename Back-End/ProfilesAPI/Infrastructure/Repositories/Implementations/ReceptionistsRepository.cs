0a44c5fcb7b43f7a5a8ca5b5c76f550ab7de268b
ig <ig@DESKTOP-PE49GG4>
2025-10-15 14:21:08 +0200

    Added IReceptionistsRepository

---
+++ Back-End/ProfilesAPI/Infrastructure/Repositories/Implementations/ReceptionistsRepository.cs
@@ -0,0 +1,65 @@
+﻿using Domain.DBServices.Models;
+using Domain.DBServices.Models.PaginationModel;
+using Infrastructure.DbConfigurations.Contexts;
+using Infrastructure.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Implementations
+{
+    public class ReceptionistsRepository : IReceptionistsRepository
+    {
+        private readonly ProfilesContext _context;
+        public ReceptionistsRepository(ProfilesContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAsync(Receptionist receptionist, CancellationToken token)
+        {
+            await _context.Receptionists.AddAsync(receptionist, token);
+        }
+
+        public async Task DeleteAsync(Receptionist receptionist, CancellationToken token)
+        {
+            _context.Remove(receptionist);
+        }
+
+        public async Task<ActionResult<PaginatedResult<Receptionist>>> GetAllAsync(PageInfo param, CancellationToken token)
+        {
+            var query = _context.Receptionists
+                .AsQueryable()
+                .AsNoTracking();
+
+            var totalRecords = await _context.Receptionists.CountAsync(token);
+
+            var receptionists = await query
+                .Skip((param.Page - 1) * param.ItemsPerPage)
+                .Take(param.ItemsPerPage)
+                .ToListAsync(token);
+
+            return new PaginatedResult<Receptionist>
+                (
+                receptionists,
+                param.Page,
+                param.ItemsPerPage,
+                totalRecords
+                );
+
+        }
+
+        public async Task<Receptionist?> GetByIdAsync(int id, CancellationToken token)
+        {
+            return await _context.Receptionists.FindAsync(id, token);
+        }
+
+        public async Task SaveDataAsync(CancellationToken token)
+        {
+            await _context.SaveChangesAsync(token);
+        }
+
+        public async Task UpdateAsync(Receptionist receptionist, CancellationToken token)
+        {
+            _context.Receptionists.Update(receptionist);
+        }
+    }
+}
---
+++ Back-End/ProfilesAPI/Infrastructure/Repositories/Interfaces/IReceptionistsRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.DBServices.Models;
+using Domain.DBServices.Models.PaginationModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Interfaces
+{
+    public interface IReceptionistsRepository
+    {
+        Task<ActionResult<PaginatedResult<Receptionist>>> GetAllAsync(PageInfo param, CancellationToken token);
+        Task<Receptionist?> GetByIdAsync(int id, CancellationToken token);
+        Task CreateAsync(Receptionist receptionist, CancellationToken token);
+        Task UpdateAsync(Receptionist receptionist, CancellationToken token);
+        Task DeleteAsync(Receptionist receptionist, CancellationToken token);
+        Task SaveDataAsync(CancellationToken token);
+    }
+}
