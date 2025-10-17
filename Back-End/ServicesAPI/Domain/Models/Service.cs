c64e40fb86ac31b8d91262c64bb083da1bb6a787
ig <ig@DESKTOP-PE49GG4>
2025-10-17 14:16:04 +0200

    Added db models

---
+++ Back-End/ServicesAPI/Domain/Models/Service.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class Service : Entity
+    {
+        public int ServiceCategoryId { get; set; }
+        public required string ServiceName { get; set; }
+        public required decimal Price { get; set; }
+        public int SpecializationId { get; set; }
+        public EVIsActive IsActive { get; set; }
+    }
+}
