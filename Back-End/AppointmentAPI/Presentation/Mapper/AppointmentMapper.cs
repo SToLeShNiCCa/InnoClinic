8d329834943f225bcbf050de68187548f32fa8f5
ig <ig@DESKTOP-PE49GG4>
2025-10-24 14:27:47 +0200

    Added DTOs

---
+++ Back-End/AppointmentAPI/Presentation/Mapper/AppointmentMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace Presentation.Mapper
+{
+    public class AppointmentMapper : Profile
+    {
+        public AppointmentMapper()
+        {
+            CreateMap<>();
+            CreateMap<>();
+
+            CreateMap<>();
+            CreateMap<>();
+
+            CreateMap<>();
+            CreateMap<>();
+        }
+    }
+}
