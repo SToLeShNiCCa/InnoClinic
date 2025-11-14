9e5641e3368a92564a7c08b3014de1b48313a545
ig <ig@DESKTOP-PE49GG4>
2025-11-14 15:02:41 +0100

    Rollback BlobRepository

--- Back-End/DocumentsAPI/Infrastructure/BlobRepository/Implementation/BlobRepository.cs
+++ Back-End/DocumentsAPI/Infrastructure/BlobRepository/Implementation/BlobRepository.cs
@@ -1,22 +1,15 @@
 ﻿using Azure.Storage.Blobs;
 using Infrastructure.BLOBRepository.Interface;
-using Microsoft.Extensions.Options;
 
 namespace Infrastructure.BLOBRepository.Implementation
 {
-    public class BlobRepository : IBlobRepository
+    public class BlobRepository(BlobServiceClient blobServiceClient) : IBlobRepository
     {
-        private readonly BlobContainerClient _containerClient;
-        public BlobRepository(IOptions<DbSettings.AzureDatabaseSettings> _settings)
-        {
-            var blobServiceClient = new BlobServiceClient(_settings.Value.AzureBlobStorage);
-            var containerClient = blobServiceClient.GetBlobContainerClient(_settings.Value.AzureBlobContainer);
-
-            _containerClient = containerClient;
-        }
+        private const string ContainerName = "photoes";
         public BlobClient AddBlobClient(Guid fileId)
         {
-            var blobClient = _containerClient.GetBlobClient(fileId.ToString());
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(ContainerName);
+            BlobClient blobClient = containerClient.GetBlobClient(fileId.ToString());
 
             return blobClient;
         }
