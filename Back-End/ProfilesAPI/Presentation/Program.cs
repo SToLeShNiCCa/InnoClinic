d93eb0991593a5701d0b065f3b3f61b2c94cd769
ig <ig@DESKTOP-PE49GG4>
2025-12-01 15:29:04 +0100

    xdghzdhfz

--- Back-End/ProfilesAPI/Presentation/Program.cs
+++ Back-End/ProfilesAPI/Presentation/Program.cs
@@ -3,7 +3,7 @@ using Infrastructure;
 using Presentation.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-//fvgbnj
+//fvgbnjfgnsfgzs
 builder.Configuration
     .AddJsonFile("appsettings.json")
     .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true);
