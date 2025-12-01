58ba6b5d38f527f27becc6c012b45b3f01c1b1c3
ig <ig@DESKTOP-PE49GG4>
2025-12-01 11:16:33 +0100

    yyy

--- Back-End/ProfilesAPI/Presentation/Program.cs
+++ Back-End/ProfilesAPI/Presentation/Program.cs
@@ -3,7 +3,7 @@ using Infrastructure;
 using Presentation.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-//i do not know what is wrong really......
+//i do not know what is wrong really....
 builder.Configuration
     .AddJsonFile("appsettings.json")
     .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true);
