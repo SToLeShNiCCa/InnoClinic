881e8ab81b337819b99599075989d38971df97e3
ig <ig@DESKTOP-PE49GG4>
2025-12-11 10:19:39 +0100

    changed network

--- Back-End/OfficesAPI/Domain/Models/Office.cs
+++ Back-End/OfficesAPI/Domain/Models/Office.cs
@@ -3,7 +3,7 @@
     public class Office : Entity
     {
         public string Address { get; set; } = default!;
-        public string PhotoID { get; set; } = default!;
+        public int PhotoID { get; set; } = default!;
         public string RegistryPhoneNumber { get; set; } = default!;
         public bool IsActive { get; set; }
     }
